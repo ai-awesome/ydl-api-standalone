bd6fc14ec4cfc8cf83571b39241f6ffd720983e2
Sila Infinity <sila083.infinity@gmail.com>
2024-06-26 16:18:57 +0700

    update release name

--- pre-release.py
+++ pre-release.py
@@ -5,7 +5,9 @@ from helper import ZIP_FILE
 
 if __name__ == "__main__":
     if os.path.exists("dist"):
-        shutil.make_archive("dist/" + ZIP_FILE, "zip", "dist")
+        temp_zip_file = "temp_" + ZIP_FILE
+        shutil.make_archive(temp_zip_file, "zip", "dist")
+        shutil.move(temp_zip_file + ".zip", "dist/" + ZIP_FILE + ".zip")
     else:
         print("dist folder not exist, please build first")
 
